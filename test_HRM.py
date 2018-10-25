ab1aa76d49f0288f0bc1e8d236ccecbfe119c976
Erica Skerrett <eskerrett11@gmail.com>
2018-10-25 16:18:07 -0400

    adds extreme finder function

---
+++ test_HRM.py
@@ -0,0 +1,11 @@
+import pytest
+
+# from HRM import read_files
+# from HRM import open_files
+
+# a = ['a.csv','b.csv','c.csv']
+# b = ['a.csv','b.xyz','b.csv','c.csv']
+
+# def test_read_files():
+#  files = read_files()
+# assert files == a
